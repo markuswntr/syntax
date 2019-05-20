29bd83b5cfca0cc2e9c526afea16a73181997bef
Markus Winter <markus@wntr.me>
2019-05-20 10:57:01 +0200

    Move configuration to tokenizer directly

--- Sources/Syntax/LexicalAnalysis/Tokenizer.swift
+++ Sources/Syntax/LexicalAnalysis/Tokenizer.swift
@@ -6,6 +6,26 @@ public struct Tokenizer {
     /// The descriptors of tokens against which to evaluate
     public let descriptors: [TokenDescriptor]
 
+    /// Configuration of a tokenizer instance.
+    ///
+    /// Use the configuration to define characters that should be ignored by the tokenizer
+    public struct Configuration {
+
+        /// The characters to be ignored while scanning for tokens.
+        ///
+        /// These characters may still appear inside token descriptions, but are never the start of a new token.
+        /// Common examples are be whitespaces and newline characters outside of strings.
+        public var ignoredCharacters: CharacterSet
+
+        /// Initializes a new configuration with given properties.
+        ///
+        /// - parameter ignoredCharacters: A set of characters to ignore while scanning for
+        ///             the start of a new token. Defaults to `.whitespaceAndNewlines`.
+        public init(ignoredCharacters: CharacterSet = .whitespacesAndNewlines) {
+            self.ignoredCharacters = ignoredCharacters
+        }
+    }
+
     /// The additional configuration of the tokenizer
     public let configuration: Configuration
 
